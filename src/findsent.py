ca1bfbb0cf32952d52065615cba1ad05b486357c
Can Liu <liucan@gizmo.cs.indiana.edu>
2013-01-21 18:11:49 -0500

    stanford_pos is working now

--- src/findsent.py
+++ src/findsent.py
@@ -4,6 +4,7 @@ we're interested in"""
 import nltk
 from nltk.corpus import brown
 from nltk.stem.porter import PorterStemmer
+from nltk.tag.stanfordtagger import StanfordTagger
 
 class FindSent:
     
@@ -36,14 +37,19 @@ class FindSent:
             if stem in self.targets and pos in nouns:
                 targets_found.append([word,stem,pos])
         return targets_found
-                
-        
+    def stanford_pos(self,sentence): 
+        """Use the stanford POS tagger, they have many pre-trained models, we can choose. Maybe later on as an argument"""
+                        
+        stanford_tagger = StanfordTagger('/nfs/nfs4/home/liucan/Desktop/Final/nltk/nltk/tag/wsj-0-18-bidirectional-distsim.tagger','/nfs/nfs4/home/liucan/Desktop/Final/nltk/nltk/tag/stanford-postagger.jar')
+        tokenized = nltk.word_tokenize(sentence)
+        tagged_sentence = stanford_tagger.tag(tokenized)
+        return tagged_sentence
 ##POS tag a sentence.
 ##Use bigram tagger from brown corpos. ??or others.
 
 
 if __name__ == "__main__":
     c = FindSent()
-    sentence = c.bigram_pos('I went to the banks')
+    sentence = c.stanford_pos('I went to the banks')
     result = c.porter_stemmer(sentence)
     print(result)
