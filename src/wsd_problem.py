a96b69efc946aefa6ffdfe53a69e21504382d8b4
Alex Rudnick <alex.rudnick@gmail.com>
2013-01-24 00:27:35 -0500

    strip <head> tags, but note which words were marked with them

--- src/wsd_problem.py
+++ src/wsd_problem.py
@@ -2,6 +2,9 @@
 
 import nltk
 
+START = "QQQSTARTHEADQQQ"
+END = "QQQENDHEADQQQ"
+
 class WSDProblem:
     """Class where we'll stash all the information about a given WSD problem."""
 
@@ -12,9 +15,19 @@ class WSDProblem:
         self.context = context
         self.head_count = count
         self.instance_id = instance_id
+        self.head_indices = []
+
+        context = context.replace("<head>", START)
+        context = context.replace("</head>", END)
 
         sentences = nltk.sent_tokenize(context)
         tokenized = [nltk.word_tokenize(sent) for sent in sentences]
+        for sent_index,sent in enumerate(tokenized):
+            for word_index,word in enumerate(sent):
+                if word.startswith(START):
+                    assert word.endswith(END)
+                    sent[word_index] = word.replace(START,"").replace(END,"")
+                    self.head_indices.append((sent_index,word_index))
         self.tokenized = tokenized
 
     def __str__(self):
