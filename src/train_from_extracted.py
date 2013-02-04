4ac30ed3dc283beef4532d4a1ceb302fecf6f67e
Alex Rudnick <alex.rudnick@gmail.com>
2013-02-04 15:18:13 -0500

    now we can train on the extracted training data

--- src/train_from_extracted.py
+++ src/train_from_extracted.py
@@ -10,8 +10,10 @@ from operator import itemgetter
 
 from nltk.classify.maxent import MaxentClassifier
 
+from wsd_problem import WSDProblem
 from parse_corpus import extract_wsd_problems
 from run_experiment import output_one_best
+from train_on_test import get_gold_answers
 import features
 
 def get_training_problems(sourceword):
@@ -19,23 +21,37 @@ def get_training_problems(sourceword):
     problems = extract_wsd_problems(fn)
     return problems
 
-def get_training_data(sourceword, target):
+def get_training_data_from_extracted(sourceword, targetlang):
     """Return a list of (featureset, label) for training."""
     out = []
-    ## map from id to labels
-    gold_answers = get_gold_answers(sourceword, target)
-    problems = get_training_problems(sourceword)
+    problems = []
+    fn = "../trainingdata/{0}.{1}.train".format(sourceword, targetlang)
 
-    ## now collate them.
+    with open(fn) as infile:
+        lines = infile.readlines()
+        lines = [line.strip() for line in lines]
+        labelss = [line.split(",") for line in lines[1::2]]
+        contexts = [line for line in lines[0::2]]
+        assert len(contexts) == len(labelss)
+
+    problemid = 0
+    answers = {}
+    for context, labels in zip(contexts, labelss):
+        for label in labels:
+            if label == '': continue
+            problem = WSDProblem(sourceword, 0, context, problemid)
+            problems.append(problem)
+            answers[problemid] = label
+            problemid += 1
+
     for problem in problems:
         theid = problem.instance_id
         featureset = features.extract(problem)
-        label = gold_answers[theid]
+        label = answers[theid]
         out.append((featureset, label))
     return out
 
 def main():
-    raise Exception("TODO(alexr): implement all of this; parse those files.")
     parser = argparse.ArgumentParser(description='clwsd')
     parser.add_argument('--sourceword', type=str, nargs=1, required=True)
     parser.add_argument('--targetlang', type=str, nargs=1, required=True)
@@ -47,7 +63,8 @@ def main():
     target = args.targetlang[0]
     sourceword = args.sourceword[0]
 
-    instances = get_training_data(sourceword, target)
+    instances = get_training_data_from_extracted(sourceword, target)
+    print("got {0} training instances!!".format(len(instances)))
     print("... training ...")
     classifier = MaxentClassifier.train(instances, trace=0, max_iter=20)
     print("LABELS", classifier.labels())
@@ -55,15 +72,14 @@ def main():
     ## with open("../eval/{0}.output".format(sourceword), "w") as outfile:
     fn = "../trialdata/alltrials/{0}.data".format(sourceword)
     problems = extract_wsd_problems(fn)
+    gold_answers = get_gold_answers(sourceword, target)
     for problem in problems:
         featureset = features.extract(problem)
         answer = classifier.classify(featureset)
+        print(problem.context)
         print(output_one_best(problem, target, answer))
         label = gold_answers[problem.instance_id]
-        print("CORRECT" if label == answer else "WRONG")
-        print("distribution was...")
-        dist = classifier.prob_classify(featureset)
-        for key in dist.samples():
-            print(" ", key, dist.prob(key))
+        print("CORRECT" if label == answer else "WRONG", end=" ")
+        print("should be:", label)
 
 if __name__ == "__main__": main()
