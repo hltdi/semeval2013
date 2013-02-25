fa257e0e13e295867d8e2b86fc948921c951c08b
Can Liu <liucan@ron.(none)>
2013-02-25 17:01:58 -0500

    before merge

--- src/get_possible_senses.py
+++ src/get_possible_senses.py
@@ -29,9 +29,9 @@ def senses(sourceword, target):
                 out.add(word)
     return out
 
-def write_data(labels,sourceword,target):
-    """Write things to a file."""
-    for 
+#def write_data(labels,sourceword,target):
+#    """Write things to a file."""
+#    for 
 
 def main():
     parser = argparse.ArgumentParser(description='clwsd')
@@ -54,12 +54,12 @@ def generate_all():
     all_target_languages = "de es fr it nl".split()
     all_words = []
     for target in all_target_languages:
-	for sourceword in all_words:
+        for sourceword in all_words:
             fileOUT = open(path + sourceword +"."+sourceword,'w')
             labels = senses(sourceword, target)
             for label in labels:
-		fileOUT.write(label + "\n")
-	    fileOUT.close()		
+                fileOUT.write(label + "\n")
+            fileOUT.close()		
 
 if __name__ == "__main__": generate_all()#main()
 
