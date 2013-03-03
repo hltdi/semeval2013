cf4d9e8c8cbf28409869e20b260a5aa3efecf57b
Alex Rudnick <alex.rudnick@gmail.com>
2013-03-03 16:40:56 -0500

    fix unicode failures in shell script

--- src/extract_all_training.sh
+++ src/extract_all_training.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
 
 LANGUAGES="nl de es fr it"
 WORDS="bank coach education execution figure job letter match mission mood  \
       movement occupation paper passage plant post pot range rest ring scene \
       side soil strain test"
 
+export PYTHONIOENCODING=utf-8
 for LANG in $LANGUAGES; do
   for WORD in $WORDS; do
     echo "extracting $LANG $WORD ..."
@@ -15,7 +19,7 @@ for LANG in $LANGUAGES; do
     --sourcetext=/space/Europarl_Intersection_preprocessed/intersection.en.txt.ascii \
     --targettext=/space/Europarl_Intersection_preprocessed/intersection."$LANG".txt \
     --alignments=/space/output_en_"$LANG"/training.align \
-    --taggerhome=/home/liucan/stanford-postagger-2012-11-11 \
+    --taggerhome=/home/liucan/stanford-postagger-2012-11-11
 
   done
 done
