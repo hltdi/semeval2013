5f1fd302f20ac6b47f5a09fafe5489d6942831ea
Alex Rudnick <alex.rudnick@gmail.com>
2013-03-06 20:35:12 -0500

    make sure we don't have the weird unicode problem we had before

--- src/run_all_l1_experiments.sh
+++ src/run_all_l1_experiments.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export PYTHONIOENCODING=utf-8
+
 LANGS="de es fr it nl"
 WORDS="coach education execution figure job letter match mission mood paper post pot range rest ring scene side soil strain test"
 
