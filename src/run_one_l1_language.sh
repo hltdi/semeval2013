23442c59e69c82f5751396ab53546a34909c5e63
Alex Rudnick <alex.rudnick@gmail.com>
2013-03-08 11:11:26 -0500

    run L1 experiments on the correct test data

--- src/run_one_l1_language.sh
+++ src/run_one_l1_language.sh
@@ -9,5 +9,5 @@ LANG=$1
 WORDS="coach education execution figure job letter match mission mood paper post pot range rest ring scene side soil strain test"
 
 for WORD in $WORDS; do
-python3 run_experiment_l1.py --targetlang="$LANG" --sourceword="$WORD" --trialdir=../trialdata/alltrials/
+python3 run_experiment_l1.py --targetlang="$LANG" --sourceword="$WORD" --trialdir=../finaltest
 done
